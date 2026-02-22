4e3df67821e92b5751700139de9bad24e379a91c
esaul <esaulazo23garcia@gmail.com>
2026-02-22 19:21:57 +0100

    b

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,22 +1,107 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
 
 const routes: Array<RouteRecordRaw> = [
+  // {
+  //   path: '/favorites',
+  //   name: 'Favorites',
+  //   component: () => import('../views/Favorites.vue')
+  // },
+  // {
+  //   path: '/cart',
+  //   name: 'Cart',
+  //   component: () => import('../views/Cart.vue')
+  // },
+  // {
+  //   path: '/settings',
+  //   name: 'Settings',
+  //   component: () => import('../views/Settings.vue')
+  // },
   {
     path: '/',
     redirect: '/home'
   },
-  {
-    path: '/home',
-    name: 'Home',
-    component: HomePage
-  }
+  
+  // {
+  //   path: '/login',
+  //   name: 'Login',
+  //   component: () => import('../views/Login.vue')
+  // },
+  // {
+  //   path: '/register',
+  //   name: 'Register',
+  //   component: () => import('../views/Register.vue')
+  // },
+  // {
+  //   path: '/restorepassword',
+  //   name: 'RestorePassword',
+  //   component: () => import('../views/RestorePassword.vue')
+  // },
+  // {
+  //   path: '/product/:id',
+  //   name: 'ProductDetail',
+  //   component: () => import('../views/ProductDetail.vue')
+  // },
+  // {
+  //   path: '/favorites',
+  //   name: 'Favorites',
+  //   component: () => import('../views/Favorites.vue')
+  // },
+  // {
+  //   path: '/purchases',
+  //   name: 'Purchases',
+  //   component: () => import('../views/Purchases.vue')
+  // },
+  // {
+  //   path: '/profilecustomer',
+  //   name: 'ProfileCustomer',
+  //   component: () => import('../views/ProfileCostumer.vue')
+  // },
+  // {
+  //   path: '/chat',
+  //   name: 'Chat',
+  //   component: () => import('../views/Chat.vue')
+  // },
+  // {
+  //   path: '/chat/:id',
+  //   name: 'ChatThread',
+  //   component: () => import('../views/ChatConversation.vue')
+  // },
+  // {
+  //   path: '/search',
+  //   name: 'Search',
+  //   component: () => import('../views/Search.vue')
+  // },
+  // {
+  //   path: '/sell',
+  //   name: 'Sell',
+  //   component: () => import('../views/Sell.vue')
+  // },
+  // {
+  //   path: '/sales',
+  //   name: 'Sales',
+  //   component: () => import('../views/Sales.vue')
+  // },
+  // {
+  //   path: '/payment',
+  //   name: 'PaymentCheck',
+  //   component: () => import('../views/PaymentCheck.vue')
+  // },
+  // {
+  //   path: '/settings',
+  //   name: 'Settings',
+  //   component: () => import('../views/Settings.vue')
+  // },
+  // {
+  //   path: '/tabs',
+  //   name: 'Tabs',
+  //   component: () => import('../views/Tabs.vue')
+  // },
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
-})
+ history: createWebHistory(process.env.BASE_URL),
+ routes,
+});
 
-export default router
+export default router;
