dbc46d688eee433b5807d2eb61c990ee6fe16099
Japhet <s.beltran@edisone.com>
2026-02-23 12:25:23 +0100

    asdasdadasds

--- frontend/src/shims-vue.d.ts
+++
@@ -1,6 +0,0 @@
-// src/shims-vue.d.ts
-declare module '*.vue' {
-  import { DefineComponent } from 'vue';
-  const component: DefineComponent<Record<string, never>, Record<string, never>, any>;
-  export default component;
-}
--- frontendtabs/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'frontendtabs',
-  webDir: 'dist'
-};
-
-export default config;
